3493e9aa3986fa5413a50691701fca68a2eb6f40
Dale Lim <ancient.mystic.wonder@gmail.com>
2017-10-06 17:30:24 +0800

    Added logout

--- src/main/java/oauthtest/config/WebSecurityConfig.java
+++ src/main/java/oauthtest/config/WebSecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.apache.commons.lang3.BooleanUtils.and;
 
 @Configuration
 @EnableOAuth2Sso
@@ -17,6 +20,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/", "/login**", "/webjars/**")
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/oauth").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 }
